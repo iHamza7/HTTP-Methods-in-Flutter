273ef9b080d1b7fcfe7ae70191d375b9534db818
iHamza7 <khanazam3324@gmail.com>
2023-03-24 11:47:30 +0500

    lib changed

--- lib/clients.dart
+++ lib/clients.dart
@@ -2,7 +2,11 @@ import 'package:http/http.dart' as http;
 
 class BaseClients {
   var client = http.Client();
-  Future<dynamic> get(String api) async {}
+  Future<dynamic> get(String api) async {
+    var url = Uri.parse(api);
+    client.get(url);
+  }
+
   Future<dynamic> post(String api) async {}
   Future<dynamic> put(String api) async {}
   Future<dynamic> del(String api) async {}
