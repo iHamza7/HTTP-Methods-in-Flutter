6853da24fa6b005ce612d0f6feaa2f1c8b9e605e
iHamza7 <khanazam3324@gmail.com>
2023-03-24 11:39:35 +0500

    changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'appbutton.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,13 +13,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF1E1E1E),
       body: SafeArea(
-          child: Column(
-        children: const [
-          FlutterLogo(
-            size: 72,
-          )
-        ],
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const FlutterLogo(
+              size: 72,
+            ),
+            AppButton(
+              operation: 'GET',
+              operationColor: Colors.lightGreen,
+              description: 'Fetch users',
+              onPressed: () {},
+            ),
+            AppButton(
+              operation: 'POST',
+              operationColor: Colors.lightGreen,
+              description: 'Fetch users',
+              onPressed: () {},
+            ),
+            AppButton(
+              operation: 'PUT',
+              operationColor: Colors.lightGreen,
+              description: 'Fetch users',
+              onPressed: () {},
+            ),
+            AppButton(
+              operation: 'DEL',
+              operationColor: Colors.lightGreen,
+              description: 'Fetch users',
+              onPressed: () {},
+            ),
+          ],
+        ),
       )),
     );
   }
